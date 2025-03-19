b00b24f760bdc365c3982b8453be34b6e683998a
Gabriel Zimmermann <gabrielnzimmermann@gmail.com>
2025-03-19 18:39:07 -0300

    Controller -> Todo

---
+++ src/main/java/com/gabrielnz/todolist/controller/TodoController.java
@@ -0,0 +1,39 @@
+package com.gabrielnz.todolist.controller;
+
+import com.gabrielnz.todolist.entity.Todo;
+import com.gabrielnz.todolist.service.TodoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+
+@RestController
+@RequestMapping("/todos")
+public class TodoController {
+    @Autowired
+    TodoService todoService;
+
+    @GetMapping
+    public ResponseEntity<List<Todo>> list(){
+        return ResponseEntity.ok().body(todoService.list());
+    }
+
+    @PostMapping
+    public ResponseEntity<List<Todo>> create(@RequestBody Todo todo) {
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{id}").buildAndExpand(todo.getId()).toUri();
+        return ResponseEntity.created(uri).body(todoService.create(todo));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<List<Todo>> update(@PathVariable Long id, @RequestBody Todo todo) {
+        return ResponseEntity.ok().body(todoService.update(id,todo));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<List<Todo>> delete(@PathVariable Long id) {
+        return ResponseEntity.ok().body(todoService.delete(id));
+    }
+}
