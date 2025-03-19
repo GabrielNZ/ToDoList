ae0a6ad2aeabbbdd646364f8a9cf638af1a32123
Gabriel Zimmermann <gabrielnzimmermann@gmail.com>
2025-03-19 18:54:29 -0300

    First Initializing / Removed Lombock

--- src/main/java/com/gabrielnz/todolist/entity/Todo.java
+++ src/main/java/com/gabrielnz/todolist/entity/Todo.java
@@ -1,23 +1,83 @@
 package com.gabrielnz.todolist.entity;
 
 import jakarta.persistence.*;
-import lombok.*;
 
-@Getter
-@Setter
-@AllArgsConstructor
-@NoArgsConstructor
-@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+import java.util.Objects;
 
 @Entity
 @Table(name = "todos")
 public class Todo {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @EqualsAndHashCode.Include
     private Long id;
     private String name;
     private String description;
     private boolean done;
     private int priority;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean isDone() {
+        return done;
+    }
+
+    public void setDone(boolean done) {
+        this.done = done;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
+
+    public void setPriority(int priority) {
+        this.priority = priority;
+    }
+
+    public Todo(Long id, String name, String description, boolean done, int priority) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.done = done;
+        this.priority = priority;
+    }
+
+    public Todo(){
+
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Todo todo = (Todo) o;
+        return Objects.equals(id, todo.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
 }
