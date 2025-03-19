b65c7c132976ac252fea5af86590b2b1661f214c
Gabriel Zimmermann <gabrielnzimmermann@gmail.com>
2025-03-19 18:11:08 -0300

    Entity -> Todo

---
+++ src/main/java/com/gabrielnz/todolist/entity/Todo.java
@@ -0,0 +1,23 @@
+package com.gabrielnz.todolist.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+
+@Entity
+@Table(name = "todos")
+public class Todo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
+    private Long id;
+    private String name;
+    private String description;
+    private boolean done;
+    private int priority;
+}
